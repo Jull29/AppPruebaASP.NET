3a24cc62f0291db03346e60ab8cc2ce084af37f9
Jull29 <agm_39_39@hotmail.com>
2015-03-02 18:58:13 -0500

    HOY

---
+++ App/Modelo/Persona.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Modelo
+{
+    public class Persona
+    {
+        string id;
+        string nombre;
+        string direccion;
+        string telefono;
+        string email;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+      
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        public string Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public Persona(string id, string nombre, string direccion, string telefono, string email);
+        public override string ToString()
+        {
+            return ToString() +
+                    "Nombre: " + this.nombre +
+                    "\nId: " + this.id + '\n' +
+                        "\nId: " + this.telefono + '\n' +
+                        "\nId: " + this.email +
+                        "\nId: " + this.direccion;
+        }
+  
+
+
+    }
+}
